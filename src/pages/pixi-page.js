47b30d2af465c28885a3c93d878dff438c8ca242
Erik Bean <Erik.Bean@nike.com>
2020-08-15 19:56:21 -0700

    use spritesheet

--- src/pages/pixi-page.js
+++ src/pages/pixi-page.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import { useEffect } from 'react';
 import { gameState } from '../mockData';
+const hand1 = gameState['coolguy@gmail.com'].hand.revealed;
+const hand2 = gameState['lamedude@hotmail.com'].hand.revealed;
 
 const suits = {
   S: 'spades',
@@ -23,62 +25,60 @@ const cardToPath = (cardString) => {
   const numericValue = cardString.substr(1);
   const suit = suits?.[cardString.charAt(0)];
   const value = withFaces(numericValue);
-  return `/png/${value}_of_${suit}.png`;
+  return `${value}_of_${suit}.png`;
 };
 
-const isAceOrFace = () => {};
-const renderCards = async (initX, initY, app, PIXI, cards) => {
-  console.log(cards);
+const renderCards = async (initX, initY, app, PIXI, cards, sheet) => {
   for (let cardString of cards) {
-    console.log(cardString);
     await new Promise((resolve, reject) => {
       const offset = 50 * cards.indexOf(cardString);
-      const resource = `card-${cardString}`;
-      app.loader.onLoad.add(resolve);
-      app.loader
-        .add(resource, cardToPath(cardString))
-        .load((loader, resources) => {
-          const card = new PIXI.Sprite(resources[resource].texture);
-          card.x = initX + offset;
-          card.y = initY;
-          card.scale.set(0.5, 0.5);
 
-          const cardBG = new PIXI.Graphics();
-          cardBG.beginFill(0xffffff);
-          cardBG.drawRoundedRect(card.x, card.y, card.width, card.height, 10);
-          cardBG.endFill();
-          cardBG.addChild(card);
-          app.stage.addChild(cardBG);
-        });
+      const card = new PIXI.Sprite(sheet.textures[cardToPath(cardString)]);
+      card.x = initX + offset;
+      card.y = initY;
+      card.scale.set(0.5, 0.5);
+
+      const cardBG = new PIXI.Graphics();
+      cardBG.beginFill(0xffffff);
+      cardBG.drawRoundedRect(card.x, card.y, card.width, card.height, 10);
+      cardBG.endFill();
+      cardBG.addChild(card);
+
+      app.stage.addChild(cardBG);
+      resolve();
     });
   }
 };
+const loadCards = (PIXI) => {
+  return new Promise((resolve, reject) => {
+    PIXI.Loader.shared
+      .add('/spritesheet.json')
+      .load(() =>
+        resolve(PIXI.Loader.shared.resources['/spritesheet.json'].spritesheet),
+      );
+  });
+};
 
-const loadPIXI = (gameState) => () => {
+const loadPIXI = () => {
   if (typeof window === 'undefined') return;
   const PIXI = require('pixi.js');
   const app = new PIXI.Application({ backgroundColor: '0xfff' });
   app.resizeTo = document.getElementById('pixi-container');
   document.getElementById('pixi-container').appendChild(app.view);
-  renderCards(
-    0,
-    0,
-    app,
-    PIXI,
-    gameState['coolguy@gmail.com'].hand.revealed,
-  ).then(() => {
-    renderCards(
-      0,
-      app.renderer.height - 200,
-      app,
-      PIXI,
-      gameState['lamedude@hotmail.com'].hand.revealed,
-    );
+
+  const renderHand1 = (sheet) => renderCards(0, 0, app, PIXI, hand1, sheet);
+  const renderHand2 = (sheet) =>
+    renderCards(0, app.renderer.height - 200, app, PIXI, hand2, sheet);
+
+  loadCards(PIXI).then((sheet) => {
+    console.log('sheet: ', sheet);
+    renderHand1(sheet);
   });
+  // .then((sheet) => renderHand2(sheet));
 };
 
 export default function Game() {
-  useEffect(loadPIXI(gameState), []);
+  useEffect(loadPIXI, []);
   return (
     <div className="page-container">
       <Head>
